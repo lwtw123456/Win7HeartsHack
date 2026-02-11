2a76437cf230152b940fab988a7899ce757ed475
lwtw123456 <jrll3035724@163.com>
2026-02-11 17:22:16 +0800

    Update controller.py

--- controller.py
+++ controller.py
@@ -1,5 +1,6 @@
 from ui import ControlWindowUi
 from heartshack import HeartsHack
+from cardselector import CardSelector
 
 class ControlWindow(ControlWindowUi):
     def __init__(self):
@@ -8,18 +9,21 @@ class ControlWindow(ControlWindowUi):
         
     def on_switch_change(self, key):
         state = self.switch_vars[key].get()
-        name_map = {
-            "free_play": "自由模式",
-            "god_mode": "无敌模式",
-            "see_all": "透视模式",
-            "exposed_hand": "明牌模式",
-            "all_two": "全二模式", 
-        }
+        name_map = {"free_play":"自由模式","god_mode":"无敌模式","see_all":"透视模式","exposed_hand":"明牌模式","self_define":"自定模式"}
         status = "开启" if state else "关闭"
-        is_success = eval(f"self.hack.{'cancel_' * (not state)}{key}()")
+        is_success = (self.open_card_selector() if state else self.hack.cancel_set_cards()) if key=="self_define" else eval(f"self.hack.{'cancel_'*(not state)}{key}()")
         self.log(f"{name_map[key]}：{status}{'' if is_success else '失败'}")
-        if not is_success:
-            self.switch_vars[key].set(not state)
+        state and is_success and key=="self_define" and self.log(f"将在下次发牌时生效！")
+        state and is_success and key=="exposed_hand" and self.log(f"将在下次发牌或换牌后生效！")
+        not is_success and self.switch_vars[key].set(not state)
+
+    def open_card_selector(self):
+        selector = CardSelector(self)
+        result = selector.show()
+        if result:
+            return self.hack.set_cards(result)
+        else:
+            return False
 
     def on_win(self):
         if self.hack.win():
